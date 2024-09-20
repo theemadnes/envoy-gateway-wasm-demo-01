85b57e157c917ae51626e28b7a09794a9add11f7
theemadnes <alex.mattson@gmail.com>
2024-09-20 06:23:10 +0000

    fixing stuff

--- ef_wasm_rust_01/src/lib.rs
+++ ef_wasm_rust_01/src/lib.rs
@@ -17,10 +17,29 @@ struct HelloWorld {
 
 impl wasm::traits::Context for HelloWorld {}
 
-impl wasm::traits::HttpContext for HelloWorld {
+/*impl wasm::traits::HttpContext for HelloWorld {
     fn on_http_request_headers(&mut self, num_headers: usize) -> wasm::types::Action {
         info!("Got {} HTTP headers in #{}.", num_headers, self.context_id);
         self.add_http_response_header("x-wasm-filter", "hello");
+
+        wasm::types::Action::Continue
+    }
+}*/
+
+impl wasm::traits::HttpContext for HelloWorld {
+    fn on_http_request_headers(&mut self, num_headers: usize) -> wasm::types::Action {
+        info!("Got {} HTTP headers in #{}.", num_headers, self.context_id);
+        let headers = self.get_http_request_headers();
+        let mut authority = "";
+
+        for (name, value) in &headers {
+            if name == ":authority" {
+                authority = value;
+            }
+        }
+
+        self.set_http_request_header("x-hello", Some(&format!("Hello world from {}", 
+authority)));
 
         wasm::types::Action::Continue
     }
