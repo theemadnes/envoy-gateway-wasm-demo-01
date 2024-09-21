e3c7b60997e66713b718bcf69bdea7c0747dfe8c
theemadnes <alex.mattson@gmail.com>
2024-09-21 03:29:55 +0000

    starting to break up filters

--- ef_wasm_rust_01/src/lib.rs
+++ ef_wasm_rust_01/src/lib.rs
@@ -48,24 +48,4 @@ authority)));
         wasm::types::Action::Continue
     }
 
-    fn on_http_response_headers(&mut self, _num_headers: usize) -> wasm::types::Action {
-        /*info!("Got {} HTTP headers in #{}.", num_headers, self.context_id);
-        let headers = self.get_http_request_headers();
-        let mut authority = "";
-
-        for (name, value) in &headers {
-            if name == ":authority" {
-                authority = value;
-            }
-        }*/
-
-        // setting a value for the x-was-filter header
-        let val1 = "hello123";
-
-        //self.set_http_request_header("x-hello", Some(&format!("Hello world from {}", 
-//authority)));
-        self.set_http_response_header("x-wasm-filter", Some(val1));
-
-        wasm::types::Action::Continue
-    }
 }
